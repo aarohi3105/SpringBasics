6caaf872281acfc5a7833dc3abed8cffda33a8ff
aarohi3105 <aarohi3101jain@gmail.com>
2026-06-02 13:54:48 +0530

    DI using constructor methods

---
+++ SpringProgram5_DI_constructormethods/src/in/sp/main/Main.java
@@ -0,0 +1,15 @@
+package in.sp.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.Student;
+
+public class Main {
+public static void main(String[] args) {
+	String config_file_loc="/in/sp/resources/applicationContext.xml";
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(config_file_loc);
+	Student std=(Student)context.getBean("stdId");
+	std.display();
+	
+}
+}
