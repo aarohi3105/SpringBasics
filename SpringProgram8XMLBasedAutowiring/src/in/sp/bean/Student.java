878c9945083162689f79135924196b4cb91d232e
aarohi3105 <aarohi3101jain@gmail.com>
2026-06-03 14:47:21 +0530

    byName autowiring

---
+++ SpringProgram8XMLBasedAutowiring/src/in/sp/bean/Student.java
@@ -0,0 +1,31 @@
+package in.sp.bean;
+
+public class Student {
+	private int rollno;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	private String name;
+	private Address address;
+	
+	public void display() {
+		System.out.println("rollno "+ rollno);
+		System.out.println("name "+name );
+		System.out.println("address"+ address);
+	}  
+}
